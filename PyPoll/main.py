cbcca7f489f9dbc86d085dedcd071ab2ed728e84
Dshashek <48182990+Dshashek@users.noreply.github.com>
2019-05-02 23:31:14 -0500

    Update main.py
    Fixed code to allow for any number of candidates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,18 @@
 import csv
 import os
 
+records = 0
+
 #open CSV
 csvpath = "election_data.csv"
 
 
 #Create Lists
 votes = []
+votetotals = []
 candidates = []
+candidatevotes = []
+candidatepercent = []
 
 #Read CSV
 with open (csvpath, newline='') as csvfile:
@@ -25,58 +30,52 @@ with open (csvpath, newline='') as csvfile:
         if candidate not in candidates:
             candidates.append(candidate) 
 
-    #Total Votes for Each Candidate
-    totalVotes = len(votes)
-    votes1 = votes.count(candidates[0])
-    votes2 = votes.count(candidates[1])
-    votes3 = votes.count(candidates[2])
-    votes4 = votes.count(candidates[3])
+    #Get Votes for each Candidate
+    counter = 0
+    for candidate in candidates:
+            items = votes.count(candidates[counter])
+            candidatevotes.append(items)
+            counter = counter + 1
 
-    #Calculate Percentage for Each Candidate
-    percent1 = "{:.3%}".format(votes1 / totalVotes)
-    percent2 = "{:.3%}".format(votes2 / totalVotes)
-    percent3 = "{:.3%}".format(votes3 / totalVotes)
-    percent4 = "{:.3%}".format(votes4 / totalVotes)
+    #Get total votes
+    totalvotes = len(votes)
 
-    #Put Candidates/Votes in a Dictionary
-    results = {
-        votes1:candidates[0],
-        votes2:candidates[1],
-        votes3:candidates[2],
-        votes4:candidates[3]
-    }
+    #Get % for each candidate
+    counter = 0
+    for candidate in candidates:
+        percentages = "{:.3%}".format(candidatevotes[counter] / totalvotes)
+        candidatepercent.append(percentages)
+        counter = counter + 1
 
-    #Find the Winner's Name from the Dictionary
-    winnerTotal = (max(results))
-
-    for votes, name in results.items():
-        if votes == winnerTotal:
-            winner = name
+    #Find the Candidate with the most votes
+    winnerTotal = max(candidatevotes)
+    winnerLoc = candidatevotes.index(winnerTotal)
+    winner = candidates[winnerLoc]
 
 #Print Results to the Terminal
+counter = 0
 print(f"Election Results")
 print(f"-------------------------")
-print(f"Total Votes: {totalVotes}")
+print(f"Total Votes: {totalvotes}")
 print(f"-------------------------")
-print(f"{candidates[0]}: {percent1} ({votes1})")
-print(f"{candidates[1]}: {percent2} ({votes2})")
-print(f"{candidates[2]}: {percent3} ({votes3})")
-print(f"{candidates[3]}: {percent4} ({votes4})")
+for candidate in candidates:
+    print(f"{candidates[counter]}: {candidatepercent[counter]} ({candidatevotes[0]})")
+    counter = counter + 1
 print(f"-------------------------")
 print(f"Winner: {winner}")
 print(f"-------------------------")
 
 #Create Text File and Print results to it
+counter = 0
 f= open("PyPol Results.txt","w")
 
 f.writelines(f"Election Results\n")
 f.writelines(f"-------------------------\n")
-f.writelines(f"Total Votes: {totalVotes}\n")
+f.writelines(f"Total Votes: {totalvotes}\n")
 f.writelines(f"-------------------------\n")
-f.writelines(f"{candidates[0]}: {percent1} ({votes1})\n")
-f.writelines(f"{candidates[1]}: {percent2} ({votes2})\n")
-f.writelines(f"{candidates[2]}: {percent3} ({votes3})\n")
-f.writelines(f"{candidates[3]}: {percent4} ({votes4}))\n")
+for candidate in candidates:
+    f.writelines(f"{candidates[counter]}: {candidatepercent[counter]} ({candidatevotes[0]})")
+    counter = counter + 1
 f.writelines(f"-------------------------\n")
 f.writelines(f"Winner: {winner}\n")
 f.writelines(f"-------------------------\n")
